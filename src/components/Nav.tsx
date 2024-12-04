b15d2e2f6f9de284c9d806be3f8987d867042670
Fabricio Laboriel <fabriciolaboriel@gmail.com>
2024-12-03 18:17:41 -0700

    finished code

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <ul>
+      <li className="nav-item">
+        <NavLink to="/" end className={({ isActive }) => (isActive ? 'nav-linnk-active' : 'nav-link')}>Home</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/SavedCandidates" className={({ isActive }) => (isActive ? 'nav-linnk-active' : 'nav-link')}>Potential Candidates</NavLink>
+      </li>
+    </ul>
   )
 };
 
--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,7 +1,58 @@
+import { useState, useEffect } from 'react';
+import Candidate from '../interfaces/Candidate.interface';
+
+
 const SavedCandidates = () => {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    try {
+    const storedCandidates = localStorage.getItem('candidate');
+    if (storedCandidates) {
+      setCandidates(JSON.parse(storedCandidates));
+      }
+      } catch (error) {
+        console.error("Failed to parse candidates from localStorage:", error);
+    }
+  }, []);
+
+  const removeFromStorage = (id: string) => {
+    const updatedCandidates = candidates.filter(candidate => candidate.id !== id);
+    console.log(candidates)
+    setCandidates(updatedCandidates);
+    localStorage.setItem('candidate', JSON.stringify(updatedCandidates));
+  }
+
   return (
     <>
       <h1>Potential Candidates</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Name (Username)</th>
+            <th>Location</th>
+            <th>Email</th>
+            <th>Company</th>
+            <th>Bio</th>
+            <th>Reject</th>
+          </tr>
+        </thead>
+        <tbody>
+        {candidates.map((candidate, index) => (
+            <tr key={index}>
+              <td><img src={candidate.avatar_url} alt={candidate.name} /></td>
+              <td>{candidate.name == null ? 'Not Available' : candidate.name} ({candidate.login == null ? 'Not Available' : candidate.login})</td>
+              <td>{candidate.location == null ? 'Not Available' : candidate.location}</td>
+              <td>{candidate.email== null ? 'Not Available' : candidate.email}</td>
+              <td>{candidate.company == null ? 'Not Available' : candidate.company}</td>
+              <td>{candidate.bio == null ? 'Not Available' : candidate.bio}</td>
+              <td><button className="button-red" onClick={() => removeFromStorage(candidate.id)}><span>-</span></button></td>
+              </tr>
+              ))}
+        </tbody>
+      </table>
+
     </>
   );
 };
