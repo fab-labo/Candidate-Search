da7c0e0ec45059a84f33c48d5b528ce076fe70ec
Fabricio Laboriel <fabriciolaboriel@gmail.com>
2024-12-17 15:41:40 -0700

    finished website features

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,30 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
+
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <ul>
-      <li className="nav-item">
-        <NavLink to="/" end className={({ isActive }) => (isActive ? 'nav-linnk-active' : 'nav-link')}>Home</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink to="/SavedCandidates" className={({ isActive }) => (isActive ? 'nav-linnk-active' : 'nav-link')}>Potential Candidates</NavLink>
-      </li>
-    </ul>
+    
+      <ul className="nav">
+        <li className="nav-item">
+          <NavLink
+          to="/"
+          end
+          className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="/SavedCandidates"
+            className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}
+            >
+              Potential Candidates
+            </NavLink>
+        </li>
+      </ul>
+    
   )
 };
 
